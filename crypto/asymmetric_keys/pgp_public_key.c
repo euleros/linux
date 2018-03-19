94a05551f7ab1b46668bc9eb4b7ab8c8abff5c29
Roberto Sassu <roberto.sassu@huawei.com>
2018-03-19 14:14:03 +0100

    KEYS: Provide PGP key description autogeneration
    Provide a facility to autogenerate the name of PGP keys from the contents
    of the payload.  If add_key() is given a blank description, a description
    is constructed from the last user ID packet in the payload data plus the
    last 8 hex digits of the key ID.  For instance:
    
           keyctl padd asymmetric "" @s </tmp/key.pub
    
    might create a key with a constructed description that can be seen in
    /proc/keys:
    
    2f674b96 I--Q---     1 perm 39390000     0     0 asymmetri \
                                Sample kernel key 31f0ae93: PGP.RSA 31f0ae93 []
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- crypto/asymmetric_keys/pgp_public_key.c
+++ crypto/asymmetric_keys/pgp_public_key.c
@@ -41,7 +41,11 @@ static inline void digest_putc(struct shash_desc *digest, uint8_t ch)
 struct pgp_key_data_parse_context {
 	struct pgp_parse_context pgp;
 	struct public_key *pub;
+	u8 *raw_fingerprint;
 	char *fingerprint;
+	const char *user_id;
+	size_t user_id_len;
+	size_t fingerprint_len;
 };
 
 /*
@@ -162,6 +166,7 @@ static int pgp_generate_fingerprint(struct pgp_key_data_parse_context *ctx,
 	if (ret < 0)
 		goto cleanup_raw_fingerprint;
 
+	ctx->fingerprint_len = digest_size * 2;
 	fingerprint = kmalloc(digest_size * 2 + 1, GFP_KERNEL);
 	if (!fingerprint)
 		goto cleanup_raw_fingerprint;
@@ -173,8 +178,10 @@ static int pgp_generate_fingerprint(struct pgp_key_data_parse_context *ctx,
 		sprintf(fingerprint + i * 2, "%02x", raw_fingerprint[i]);
 	pr_debug("fingerprint %s\n", fingerprint);
 
+	ctx->raw_fingerprint = raw_fingerprint;
 	ctx->fingerprint = fingerprint;
 	ret = 0;
+	goto cleanup_hash;
 cleanup_raw_fingerprint:
 	kfree(raw_fingerprint);
 cleanup_hash:
@@ -203,6 +210,13 @@ static int pgp_process_public_key(struct pgp_parse_context *context,
 	int ret;
 
 	kenter(",%u,%u,,%zu", type, headerlen, datalen);
+
+	if (type == PGP_PKT_USER_ID) {
+		ctx->user_id = data;
+		ctx->user_id_len = datalen;
+		kleave(" = 0 [user ID]");
+		return 0;
+	}
 
 	if (ctx->fingerprint) {
 		kleave(" = -ENOKEY [already]");
@@ -262,28 +276,81 @@ cleanup:
 static int pgp_key_parse(struct key_preparsed_payload *prep)
 {
 	struct pgp_key_data_parse_context ctx;
+	struct asymmetric_key_ids *kids;
+	struct asymmetric_key_id *kid;
 	int ret;
 
 	kenter("");
 
 	memset(&ctx, 0, sizeof(ctx));
-	ctx.pgp.types_of_interest = (1 << PGP_PKT_PUBLIC_KEY);
+	ctx.pgp.types_of_interest = (1 << PGP_PKT_PUBLIC_KEY) |
+				    (1 << PGP_PKT_USER_ID);
 	ctx.pgp.process_packet = pgp_process_public_key;
 
 	ret = pgp_parse_packets(prep->data, prep->datalen, &ctx.pgp);
 	if (ret < 0)
 		goto error;
 
+	if (ctx.user_id && ctx.user_id_len > 0) {
+		/* Propose a description for the key
+		 * (user ID without the comment)
+		 */
+		size_t ulen = ctx.user_id_len, flen = ctx.fingerprint_len;
+		const char *p;
+
+		p = memchr(ctx.user_id, '(', ulen);
+		if (p) {
+			/* Remove the comment */
+			do {
+				p--;
+			} while (*p == ' ' && p > ctx.user_id);
+			if (*p != ' ')
+				p++;
+			ulen = p - ctx.user_id;
+		}
+
+		if (ulen > 255 - 9)
+			ulen = 255 - 9;
+		prep->description = kmalloc(ulen + 1 + 8 + 1, GFP_KERNEL);
+		ret = -ENOMEM;
+		if (!prep->description)
+			goto error;
+		memcpy(prep->description, ctx.user_id, ulen);
+		prep->description[ulen] = ' ';
+		memcpy(prep->description + ulen + 1,
+		       ctx.fingerprint + flen - 8, 8);
+		prep->description[ulen + 9] = 0;
+		pr_debug("desc '%s'\n", prep->description);
+	}
+
+	kid = asymmetric_key_generate_id(ctx.raw_fingerprint,
+					 ctx.fingerprint_len / 2,
+					 NULL, 0);
+	if (IS_ERR(kid))
+		goto error;
+
+	kids = kmalloc(sizeof(struct asymmetric_key_ids), GFP_KERNEL);
+	if (!kids)
+		goto error_kid;
+
+	kids->id[0] = kid;
+	kids->id[1] = kmemdup(kid, sizeof(kid) + (ctx.fingerprint_len / 2),
+			      GFP_KERNEL);
+
 	/* We're pinning the module by being linked against it */
 	__module_get(public_key_subtype.owner);
 	prep->payload.data[asym_subtype] = &public_key_subtype;
+	prep->payload.data[asym_key_ids] = kids;
 	prep->payload.data[asym_crypto] = ctx.pub;
 	prep->quotalen = 100;
 	return 0;
 
+error_kid:
+	kfree(kid);
 error:
 	public_key_free(ctx.pub);
 	kfree(ctx.fingerprint);
+	kfree(ctx.raw_fingerprint);
 	return ret;
 }
 
